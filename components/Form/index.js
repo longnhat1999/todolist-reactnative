a2f3b68bbf5ddc4a595d479b000e55d4e0244fa3
Long Nhat <nhatodin20041999@gamil.com>
2022-07-06 17:53:21 +0700

    handle-add-task

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,16 +1,29 @@
 import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './style'
 
 const Form = () => {
+  const [task, setTask] = useState('')
+  const handleAddTask= () => {
+    if(task.length === 0){
+      alert('Vui lòng nhập công việc!')
+      return false;
+    }
+    alert(task);
+  }
   return (
     <KeyboardAvoidingView 
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={10}
       style={styles.addTask}
     >
-      <TextInput placeholder='Your Task' style={styles.input}/>
-      <TouchableOpacity>
+      <TextInput 
+        placeholder='Your Task'
+        onChangeText={setTask}
+        // onChangeText={(text) => setTask(text)} 
+        style={styles.input}
+      />
+      <TouchableOpacity onPress={handleAddTask}>
         <View style={styles.iconWrapper}>
           <Text style={styles.icon}>+</Text>
         </View>
