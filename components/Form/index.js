ea2ccc379c8520b6271e0d4c39db7bbf63aaa5c8
Long Nhat <nhatodin20041999@gamil.com>
2022-07-06 17:31:29 +0700

    split-color2

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,15 +1,21 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 import styles from './style'
 
 const Form = () => {
   return (
-    <View style={styles.addTask}>
-      <TextInput style={styles.input}/>
-      <View style={styles.iconWrapper}>
-        <Text style={styles.icon}>+</Text>
-      </View>
-    </View>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={10}
+      style={styles.addTask}
+    >
+      <TextInput placeholder='Your Task' style={styles.input}/>
+      <TouchableOpacity>
+        <View style={styles.iconWrapper}>
+          <Text style={styles.icon}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   )
 }
 
